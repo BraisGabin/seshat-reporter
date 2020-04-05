e5f749bca8e6fddcbc44f65a75412ac184a30f2f
Brais Gabín <braisgabin@gmail.com>
2020-04-05 20:41:18 +0200

    Improve reporter

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,15 @@ set -ex
 cd "${GITHUB_WORKSPACE}"
 
 diff_file=$(mktemp)
-git diff >${diff_file}
 
-git log >asdf
-cat asdf
+sha=$((git show -s --format=%B | sed -n -E 's/^Merge ([0-9abcdef]{40}) into [0-9abcedef]{40}$/\1/; t; q1' || echo ${GITHUB_SHA}) | tail -n1)
+
+echo ${sha}
+echo ${sha}
+echo ${sha}
+echo ${sha}
+
+git diff >${diff_file}
 
 pull_number=$(jq --raw-output .pull_request.number "${GITHUB_EVENT_PATH}")
 curl --data-binary "@${diff_file}" \
